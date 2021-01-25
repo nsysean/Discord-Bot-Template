98220e6c2e7264519de6801e3eb615663c1bd0b9
nsysean <76026698+nsysean@users.noreply.github.com>
2021-01-25 14:57:17 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const {Collection, Client, Discord} = require('discord.js')
+const fs = require('fs')
+const client = new Client({
+    disableEveryone: true
+})
+module.exports = client;
+const config = require('./config.json')
+const prefix = ("x")
+const token = config.token
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+}); 
+
+client.login(token)
